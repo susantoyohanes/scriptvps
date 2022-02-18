056585e4fa835004d51884bf33a169542fa2d95c
susantoyohanes <99692248+susantoyohanes@users.noreply.github.com>
2022-02-18 10:05:20 +0700

    Add files via upload

---
+++ ssh/about.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#     Premium Auto Script By NATANVPN           #"
+echo -e "================================================="
+echo -e "# For Debian 10 64 bit                          #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By NATANVPN                          #"
+echo -e "================================================="
