863d4f107d2904bf8520e2f48f9ed9d24522a09e
susantoyohanes <99692248+susantoyohanes@users.noreply.github.com>
2022-03-12 02:42:39 +0700

    Update addvless.sh

--- xray/addvless.sh
+++ xray/addvless.sh
@@ -52,8 +52,8 @@ sed -i '/#xray-vless-tls$/a\#### '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
 sed -i '/#xray-vless-nontls$/a\#### '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
-xrayvless1="vless://${uuid}@${domain}:$tls?path=/vless/&security=tls&encryption=none&type=ws#${user}"
-xrayvless2="vless://${uuid}@${domain}:$nontls?path=/vless/&encryption=none&type=ws#${user}"
+xrayvless1="vless://${uuid}@${domain}:$tls?path=/natanssh&security=tls&encryption=none&type=ws#${user}"
+xrayvless2="vless://${uuid}@${domain}:$nontls?path=/natanssh&encryption=none&type=ws#${user}"
 systemctl restart xray.service
 service cron restart
 clear
@@ -67,7 +67,7 @@ echo -e "Port No TLS : $nontls"
 echo -e "User ID     : ${uuid}"
 echo -e "Encryption  : none"
 echo -e "Network     : ws"
-echo -e "Path        : /vless/"
+echo -e "Path        : /natanssh"
 echo -e "Created     : $hariini"
 echo -e "Expired     : $exp"
 echo -e "========================="
