fa29813f33a04c839e2efc5dbdb961fc6f0d4f5e
susantoyohanes <99692248+susantoyohanes@users.noreply.github.com>
2022-06-04 19:05:03 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -42,16 +42,7 @@ akbarvpnnnnnnnnnn="raw.githubusercontent.com/susantoyohanes/scriptvps/main/ohp"
 # Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
 echo "Checking VPS"
-IZIN=$( curl https://raw.githubusercontent.com/susantoyohanes/perizinan/main/ipvps.txt )
-if [ $MYIP = $IZIN ]; then
-echo -e "${NC}${GREEN}Permission Accepted...${NC}"
-else
-echo -e "${NC}${RED}Permission Denied!${NC}";
-echo -e "${NC}${LIGHT}Please Contact Admin!!"
-echo -e "${NC}${LIGHT}Instagram : https://www.instagram.com/susantoyohanes1/"
-echo -e "${NC}${LIGHT}WhatsApp : 082260009781"
-echo -e "${NC}${LIGHT}Telegram : https://t.me/susantoyohanes69"
-exit 0
+IZIN=$(wget -qO- ipinfo.io/ip);
 
 rm -f setup.sh
 clear
